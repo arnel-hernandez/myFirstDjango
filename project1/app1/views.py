0854a21e58460a2505d31087b234ea6b5033a6a5
Arnel <aarnelhernandez@gmail.com>
2020-03-15 18:19:45 -0500

    Added Data

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from app1.models import AccessRecord,Topic,Webpage
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    my_dict = {'insert_me':"Hello From Views.py"}
+    return render(request,'app1/index.html',context=date_dict)
--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from app1 import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('app1/',include('app1.urls')),
     path('admin/', admin.site.urls),
 ]
